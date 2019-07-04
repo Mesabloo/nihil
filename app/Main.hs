730829e83376d439b41831a2e5331afcb55f3ce1
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-07-04 10:04:57 +0200

    :lipstick: Added a little CLI

--- app/Main.hs
+++ app/Main.hs
@@ -4,6 +4,31 @@ module Main
 ) where
 
 import Blob.REPL.REPL
+import qualified Blob.REPL.Defaults as Def
+import Options.Applicative
 
 main :: IO ()
-main = runREPL replLoop
+main = execCommand =<< customExecParser p opts
+  where opts = info (commands <**> helper) fullDesc
+        p = prefs showHelpOnError
+
+data Options = Options
+    { subCommand :: Command
+    , version :: Bool }
+    deriving Show
+
+data Command =
+    REPL [FilePath]
+    deriving Show
+
+
+execCommand :: Options -> IO ()
+execCommand (Options (REPL _) True) = putStrLn $ "iBlob v" <> Def.version
+execCommand (Options (REPL fs) _) = customRunREPL replLoop (REPLOptions fs)
+
+commands :: Parser Options
+commands = hsubparser
+    ( command "repl" (info replOption ( progDesc "Run blob's REPL" )) )
+
+replOption :: Parser Options
+replOption = Options <$> (REPL <$> many (strArgument (metavar "FILES..."))) <*> switch ( long "version" <> short 'v' <> help "Print the version" )
