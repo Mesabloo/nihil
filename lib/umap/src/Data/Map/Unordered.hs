aa713b345b387cce6623ce40c53bbfff9bc653a8
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-07-30 22:27:51 +0200

    :recycle: Cleaning re-exports

--- lib/umap/src/Data/Map/Unordered.hs
+++ lib/umap/src/Data/Map/Unordered.hs
@@ -3,17 +3,9 @@
 -- | An unordered, strict map.
 
 module Data.Map.Unordered
-    ( Map, empty, lookup, insert, delete, fromList, toList, map, singleton, insertWith
-    , member, elems, unionWith, difference, union, size, null
-    , intersection, foldrWithKey, foldlWithKey', keys
-    , adjust, update, alter
-    , unions, toHashMap, fromHashMap, filter, filterWithKey, keys, lookupDefault
-    , mapMaybe, mapWithKey
-    )
-where
-
+( Map
+, module Data.HashMap.Strict.InsOrd ) where
 
 import Data.HashMap.Strict.InsOrd
-
 
 type Map = InsOrdHashMap
