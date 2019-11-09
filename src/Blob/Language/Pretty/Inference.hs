0d52d1b56f2bc898d546b3a32f4d52333ff1901c
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2019-11-09 12:06:37 +0100

    :art: Improved pretty printing of types

--- src/Blob/Language/Pretty/Inference.hs
+++ src/Blob/Language/Pretty/Inference.hs
@@ -36,14 +36,11 @@ pType (t :- _) = case t of
     TFloat -> text "Double"
     TChar -> text "Char"
     TTuple ts -> parens . mconcat $ intersperse (text ", ") (map (pType . flip (:-) Nothing) ts)
-    TFun (t1, l) t2 -> parenthesizeIfNeededF t1 <> text "|" <> text (show l) <> text "|" <+> text "->" <+> pType (t2 :- Nothing)
+    TFun (t1, l) t2 -> parenthesizeIfNeeded t1 <> text "|" <> text (show l) <> text "|" <+> text "->" <+> pType (t2 :- Nothing)
     TApp t1 t2 -> pType (t1 :- Nothing) <+> parenthesizeIfNeeded t2
   where parenthesizeIfNeeded t = case t of
             TFun _ _ -> parens $ pType (t :- Nothing)
             TApp _ _ -> parens $ pType (t :- Nothing)
-            _ -> pType (t :- Nothing)
-        parenthesizeIfNeededF t = case t of
-            TFun _ _ -> parens $ pType (t :- Nothing)
             _ -> pType (t :- Nothing)
 
 -- | Kind pretty printing
