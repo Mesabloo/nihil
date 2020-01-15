00047347144af5da533cfe3236a6bcc0b0007cdb
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-01-15 19:18:52 +0100

    :sparkles: Added a `main` evaluator

--- app/nihilin/Main.hs
+++ app/nihilin/Main.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE BlockArguments #-}
 
 module Main
 ( main ) where
@@ -7,16 +8,20 @@ module Main
 import Nihil
 import Nihil.Utils.Debug
 import Nihil.Utils.Source
-import Prelude hiding (log, error)
+import Prelude hiding (log, error, lookup)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Text.PrettyPrint.ANSI.Leijen (Doc, text, red)
 import System.Exit
 import Data.Bifunctor (first)
 import qualified Text.Megaparsec as MP
-import Control.Monad.Except (ExceptT, runExceptT, liftEither)
+import Control.Monad.Except (ExceptT, runExceptT, liftEither, throwError)
 import Control.Lens ((%~), (^.))
 import Control.Monad.IO.Class (liftIO)
+import Control.Monad (when)
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import Data.Maybe (isNothing)
 
 main :: IO ()
 main = (runExceptT . workWith =<< T.getContents) >>= \case
@@ -30,18 +35,25 @@ workWith input = do
     !ast     <- log "Parsing code..."      $ liftEither (first mpErrorToDoc (runParser lexemes filename))
     !dast    <- log "Desugaring AST..."    $ liftEither (first text (runDesugarer ast))
     _        <- log "Typechecking code..." $ liftEither (runTypeChecker defaultGlobalEnv dast)
-    log "Evaluating code..." $ eval defaultEvalEnv dast
+    let !env = addToEnv defaultEvalEnv dast
+
+    when (isNothing (lookup "main" (env ^. vals))) do
+        throwError (text "Function \"main\" not found.")
+
+    val      <- log "Evaluating code..."   $ liftEither =<< liftIO (evaluate (dummyPos'ed (EId "main")) env)
+    info (pretty val) (pure ())
   where mpErrorToDoc :: (MP.Stream s, MP.ShowErrorComponent e) => MP.ParseErrorBundle s e -> Doc
         mpErrorToDoc = text . MP.errorBundlePretty
 
-        eval :: EvalState -> Program -> ExceptT Doc IO ()
-        eval _ (Program [])       = pure ()
-        eval env (Program (s:ss)) = case annotated s of
-            FunctionDefinition name ex -> do
-                let inEnv = (vals %~ insert (name, lam name (env ^. vals))) env
-                val <- liftEither =<< liftIO (evaluate ex inEnv)
-                eval ((vals %~ insert (name, val)) inEnv) (Program ss)
-            _                          -> eval env (Program ss)
+        addToEnv :: EvalState -> Program -> EvalState
+        addToEnv env (Program [])     = env
+        addToEnv env (Program (s:ss)) = case annotated s of
+            FunctionDefinition name ex -> addToEnv ((vals %~ insert (name, VUnevaluated ex)) env) (Program ss)
+            TypeDefinition _ _ cty     -> case annotated cty of
+                SumType ctors -> do
+                    let newEnv = Set.fromList (Map.keys ctors)
+                    addToEnv ((cons %~ (<>) newEnv) env) (Program ss)
+                _             -> addToEnv env (Program ss)
+            _                          -> addToEnv env (Program ss)
 
         dummyPos'ed = (`locate` NoSource)
-        lam name = VLambda (dummyPos'ed (PId "x")) (dummyPos'ed (EApplication (dummyPos'ed (EId name)) (dummyPos'ed (EId "x"))))
