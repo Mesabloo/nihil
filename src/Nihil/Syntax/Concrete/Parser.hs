24c18b9716eb25b491fe6399ea9f8e83f343d997
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-02-08 10:59:40 +0100

    :boom: Define multiple useful parser for indentation-sensitive parsing

--- src/Nihil/Syntax/Concrete/Parser.hs
+++ src/Nihil/Syntax/Concrete/Parser.hs
@@ -3,104 +3,21 @@
 module Nihil.Syntax.Concrete.Parser
 ( -- * Getting source position
   getSourcePos, withPosition
-  -- * Adjusting indentation levels
-, sameLineOrIndented, sameIndentLevel, sameColumn, sameLineOrColumn, nonIndented ) where
+  -- * Indentation-sensitive parsing
+, nonIndented, indentBlock, lineFold, lexeme, spacen1, MPL.IndentOpt(..) ) where
 
 import Nihil.Syntax.Common (Parser)
 import Nihil.Utils.Source
-import Nihil.Utils.Debug
-import Data.Functor ((<&>))
-import Control.Lens ((^.))
+import Nihil.Syntax.Concrete.Parser.Comment
 import qualified Text.Megaparsec as MP
-import Control.Monad (unless, guard)
-import Control.Applicative ((<|>))
+import qualified Text.Megaparsec.Char as MPC
+import qualified Text.Megaparsec.Char.Lexer as MPL
+import Control.Monad (void)
+import qualified Data.Char as Ch
 
 -- | Gets the source position of the next token, unwrapping it from the 'Nihil.Utils.Source.Located' data type.
 getSourcePos :: Parser SourcePos
-getSourcePos = MP.try (MP.lookAhead MP.anySingle) <&> location
-
--- | Checks if a parser is eligible for being runned.
---
---   The parser will be run only if the position retrieved via 'getSourcePos' is either
---
---   - on the same line than the position passed
---
---   - on a further line, but more indented than the position passed.
---
---   If both cases fail, this parser alos 'fail's.
-sameLineOrIndented :: SourcePos -> Parser a -> Parser a
-sameLineOrIndented pos parser = do
-    pos2 <- getSourcePos
-
-    warnWhen (pos == NoSource || pos2 == NoSource) "Unsourced tokens may be hard to keep track of!" (pure ())
-
-    unless (pos ^. sourceLine == pos2 ^. sourceLine) do
-        let indent = pos ^. indentLevel
-        guard (pos2 ^. indentLevel > indent)
-            <|> fail ("Possible incorrect indentation (should be greater than " <> show indent <> ")")
-    parser
-
--- | Checks if a parser is eligible for being runned.
---
---   The parser will be run only if the position of the next token is on the same indent level as
---   the position passed.
---
---   If not, this parser 'fail's.
-sameIndentLevel :: SourcePos -> Parser a -> Parser a
-sameIndentLevel pos parser = do
-    pos2 <- getSourcePos
-
-    warnWhen (pos == NoSource || pos2 == NoSource) "Unsourced tokens may be hard to keep track of!" (pure ())
-
-    let indent = pos ^. indentLevel
-    guard (pos2 ^. indentLevel == indent)
-        <|> fail ("Possible incorrect indentation (should equal " <> show indent <> ")")
-    parser
-
--- | This parser is similar to 'sameIndentLevel' but it checks for column alignment instead of indentation level.
-sameColumn :: SourcePos -> Parser a -> Parser a
-sameColumn pos parser = do
-    pos2 <- getSourcePos
-
-    warnWhen (pos == NoSource || pos2 == NoSource) "Unsourced tokens may be hard to keep track of!" (pure ())
-
-    let column = pos ^. sourceColumn
-    guard (pos2 ^. sourceColumn == column)
-        <|> fail ("Possible incorrect alignment (should be on column " <> show column <> ")")
-    parser
-
--- | This parser is analogous to 'sameLineOrIndented', except that it checks for the column alignment
---   instead of indentation levels.
-sameLineOrColumn :: SourcePos -> Parser a -> Parser a
-sameLineOrColumn pos parser = do
-    pos2 <- getSourcePos
-
-    warnWhen (pos == NoSource || pos2 == NoSource) "Unsourced tokens may be hard to keep track of!" (pure ())
-
-    unless (pos ^. sourceLine == pos2 ^. sourceLine) do
-        let column = pos ^. sourceColumn
-        guard (pos2 ^. sourceColumn == column)
-            <|> fail ("Possible incorrect alignment (should be on column " <> show column <> ")")
-    parser
-
--- | Checks if a parser is eligible for being runned.
---
---   The parser will be run only if the position retrieved has an indentation level of 0.
---
---   You may define this parser as
---
---   @'nonIndented' parser = do
---    pos <- 'getSourcePos'
---    'sameIndentLevel' (pos . 'indentLevel' 'Control.Lens..~' 0) parser @
-nonIndented :: Parser a -> Parser a
-nonIndented parser = do
-    pos2 <- getSourcePos
-
-    warnWhen (pos2 == NoSource) "Unsourced tokens may be hard to keep track of!" (pure ())
-
-    guard (pos2 ^. indentLevel == 0)
-        <|> fail "Possible incorrect indentation (should equal 0)"
-    parser
+getSourcePos = fromSourcePos <$> MP.getSourcePos
 
 -- | Runs a parser and annotates its result with its beginning parsing position.
 withPosition :: Parser a -> Parser (Located a)
@@ -108,3 +25,36 @@ withPosition parse = do
     pos <- getSourcePos
     a <- parse
     pure (locate a pos)
+
+isSpace :: Char -> Bool
+isSpace c =
+    let code = Ch.ord c
+    in code == 9 || code == 32 || code == 160 || code == 8200 || code == 8201 || code == 8202
+
+space1 :: Parser ()
+space1 = MPL.space sc1 pLineComment pBlockComment
+
+spacen1 :: Parser ()
+spacen1 = MPL.space scn1 pLineComment pBlockComment
+
+sc1 :: Parser ()
+sc1 = void (MP.satisfy isSpace)
+
+scn1 :: Parser ()
+scn1 = void MPC.spaceChar
+
+-- | See @'MPL.nonIndented'@.
+nonIndented :: Parser a -> Parser a
+nonIndented = MPL.nonIndented spacen1
+
+-- | See @'MPL.indentBlock'@.
+indentBlock :: Parser (MPL.IndentOpt Parser a b) -> Parser a
+indentBlock = MPL.indentBlock spacen1
+
+-- | See @'MPL.lineFold'@.
+lineFold :: (Parser () -> Parser a) -> Parser a
+lineFold = MPL.lineFold spacen1
+
+-- | See @'MPL.lexeme'@.
+lexeme :: Parser a -> Parser a
+lexeme = MPL.lexeme space1
