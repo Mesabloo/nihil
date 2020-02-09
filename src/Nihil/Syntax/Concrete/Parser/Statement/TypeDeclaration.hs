ba6d5cdbc7a02df90d7e1ecf4e677e8674312692
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-02-09 12:28:20 +0100

    :sparkles: Parse indented top-level statements

--- src/Nihil/Syntax/Concrete/Parser/Statement/TypeDeclaration.hs
+++ src/Nihil/Syntax/Concrete/Parser/Statement/TypeDeclaration.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE BlockArguments #-}
 
 module Nihil.Syntax.Concrete.Parser.Statement.TypeDeclaration
@@ -10,47 +11,43 @@ import Nihil.Syntax.Concrete.Parser.Keyword
 import Nihil.Syntax.Concrete.Parser.Identifier
 import Nihil.Syntax.Concrete.Parser.Type.Atom
 import Nihil.Syntax.Concrete.Parser.Type
-import Nihil.Syntax.Concrete.Parser.Enclosed
 import Nihil.Utils.Source
 import Nihil.Syntax.Concrete.Debug
-import Nihil.Utils.Debug
 import qualified Text.Megaparsec as MP
 import qualified Data.Map as Map
-import Control.Applicative ((<|>))
 import Prelude hiding (log)
 
 pDataType :: Parser AStatement
 pDataType = debug "pDataType" $ withPosition do
     pKeyword "data"
-    pos  <- getSourcePos
-    name <- sameLineOrIndented pos pIdentifier'
-    tvs  <- MP.many (sameLineOrIndented pos pIdentifier)
+    lineFold \spaces -> lexeme do
+        name <- annotated <$> (MP.try spaces *> pIdentifier')
+        tvs <- fmap annotated <$> MP.many (MP.try spaces *> pIdentifier)
+        MP.try spaces *> (TypeDefinition name tvs <$> MP.choice
+            [ pADT spaces, pGADT spaces ])
 
-    cTy  <- MP.choice
-        [ sameLineOrIndented pos (pSymbol "=") *> pADT pos
-        , sameLineOrIndented pos (pKeyword "where") *> (pBraces (pGADT pos) <|> pGADT pos) ]
-    pure (TypeDefinition name tvs cTy)
-
-pADT :: SourcePos -> Parser ACustomType
-pADT initPos = debug "pADT" $ withPosition do
-    let ~constructor = (,) <$> pIdentifier' <*> MP.many (sameLineOrIndented initPos pAtom)
-    log "pADT test" (pure ())
-    ctors <- sameLineOrIndented initPos constructor `MP.sepBy1` sameLineOrIndented initPos (pSymbol "|")
+pADT :: Parser () -> Parser ACustomType
+pADT s = debug "pADT" $ withPosition do
+    MP.try s *> pSymbol' "="
+    let ~constructor = (,) <$> (annotated <$> pIdentifier') <*> MP.many (MP.try s *> pAtom s)
+    ctors <- constructor `MP.sepBy1` (MP.try s *> pSymbol' "|" <* MP.try s)
     pure (SumType (Map.fromList ctors))
 
-pGADT :: SourcePos -> Parser ACustomType
-pGADT initPos = debug "pGADT" $ withPosition do
-    pos <- getSourcePos
-    let ~constructor = (,) <$> sameLineOrIndented initPos pIdentifier' <*> (sameLineOrIndented initPos (pSymbol ":") *> sameLineOrIndented initPos pType)
-    ctors <- MP.some (sameLineOrColumn pos constructor <* MP.many (pSymbol ";"))
-    pure (GADT (Map.fromList ctors))
+pGADT :: Parser () -> Parser ACustomType
+pGADT s = debug "pGADT" $ withPosition do
+    indentBlock do
+        let ~constructor = lineFold \s -> do
+                (,) <$> (annotated <$> pIdentifier') <*> ((MP.try s *> pSymbol' ":") *> (MP.try s *> pType s))
+
+        pKeyword "where"
+        pure (IndentSome Nothing (pure . GADT . Map.fromList) constructor)
 
 pTypeAlias :: Parser AStatement
 pTypeAlias = debug "pTypeAlias" $ withPosition do
-    pos  <- getSourcePos
-    pKeyword "type"
-    name <- sameLineOrIndented pos pIdentifier'
-    tvs  <- MP.many (sameLineOrIndented pos pIdentifier)
-    sameLineOrIndented pos (pSymbol "=")
-    ty   <- withPosition (TypeAlias <$> sameLineOrIndented pos pType)
-    pure (TypeDefinition name tvs ty)
+    lineFold \s -> do
+        pKeyword "type"
+        name <- annotated <$> (MP.try s *> pIdentifier')
+        tvs  <- fmap annotated <$> MP.many (MP.try s *> pIdentifier)
+        MP.try s *> pSymbol' "="
+        ty   <- withPosition (TypeAlias <$> (MP.try s *> pType s))
+        pure (TypeDefinition name tvs ty)
