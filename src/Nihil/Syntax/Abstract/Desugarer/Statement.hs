ff3d3dcc238222d17a38720bf9fb52efb6e77da0
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-02-21 14:09:25 +0100

    :bug: Fix desugaring of typeclasses to avoid undefined kind errors

--- src/Nihil/Syntax/Abstract/Desugarer/Statement.hs
+++ src/Nihil/Syntax/Abstract/Desugarer/Statement.hs
@@ -84,14 +84,14 @@ desugarStatement s ss =
                     in locate (AC.TApplication acc (transformLocated AC.TVar t)) pos
             let classType = foldl locate' (transformLocated AC.TId name) params
 
-            let addConstraint = hoistAnnotated (first plusConst)
+            let -- addConstraint = hoistAnnotated (first plusConst)
                 plusConst (AC.FunctionDeclaration name ty) =
                     let pos = location ty
                     in AC.FunctionDeclaration name (locate (AC.Implements classType ty) pos)
                 plusConst g = g
 
             AC.Program stts <- desugarProgram (CC.Program stts)
-            pure (Just (AC.ClassDefinition (AC.Forall (annotated <$> params) classType) (addConstraint <$> stts)), ss)
+            pure (Just (AC.ClassDefinition (AC.Forall (annotated <$> params) classType) {- (addConstraint <$> stts) -} stts), ss)
         CC.InstanceDefinition (name, args) defs -> do
             let transformLocated f = hoistAnnotated (first f)
                 locate' acc t = do
