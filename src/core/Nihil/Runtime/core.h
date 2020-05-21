da6f52803f382e7df2fbed10bb2ab78c25a4b675
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-05-21 22:51:43 +0200

    :recycle: Rewrite the runtime core
    The goal is to target a C stub, to allow for interfacing with some Rust code

---
+++ src/core/Nihil/Runtime/core.h
@@ -0,0 +1,78 @@
+#pragma once
+#ifndef NIHIL_RUNTIME_CORE_H
+#define NIHIL_RUNTIME_CORE_H
+
+#include <stddef.h>
+
+struct VPattern_s;
+struct VExpr_s;
+struct VBranch_s;
+struct VDecl_s;
+
+struct VPattern_s
+{
+    enum VPattern_Cons
+    {
+        CrPInteger,
+        CrPDouble,
+        CrPCharacter,
+        CrPId,
+        CrPWildcard,
+        CrPTuple,
+        CrPConstructor
+    } ctor;
+
+    union
+    {
+        struct { long p_i; } pInteger;
+        struct { double p_d; } pDouble;
+        struct { char p_c; } pCharacter;
+        struct { const char *p_name; } pId;
+        struct { unsigned long n; struct VPattern_s **p_patterns; } pTuple;
+        struct { const char *p_name; unsigned long n; struct VPattern_s **p_args; } pConstructor;
+    } value;
+} VPattern;
+
+struct VExpr_s
+{
+    enum VExpr_Cons
+    {
+        CrEId,
+        CrEInteger,
+        CrEDouble,
+        CrECharacter,
+        CrELambda,
+        CrEApplication,
+        CrETuple,
+        CrETypeHole,
+        CrEMatch,
+        CrELet
+    } ctor;
+
+    union
+    {
+        struct { const char *v_name; } eId;
+        struct { long v_i; } eInteger;
+        struct { double v_d; } eDouble;
+        struct { char v_c; } eCharacter;
+        struct { struct VPattern_s *v_arg; struct VExpr_s *v_ex; } eLambda;
+        struct { struct VExpr_s *v_fun; struct VExpr_s *v_x; } eApplication;
+        struct { unsigned long n; struct VExpr_s **v_vals; } eTuple;
+        struct { struct VExpr_s *v_expr; unsigned long n; struct VBranch_s **v_branches; } eMatch;
+        struct { unsigned long n; struct VDecl_s **v_decls; struct VExpr_s *v_expr; } eLet;
+    } value;
+};
+
+struct VBranch_s
+{
+    struct VPattern_s *b_pattern;
+    struct VExpr_s *b_expr;
+};
+
+struct VDecl_s
+{
+    const char *d_name;
+    struct VExpr_s *d_val;
+};
+
+#endif
