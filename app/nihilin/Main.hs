e727fb40f1dfa6ea7718ff6aa6e5c9d3ddc8493b
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-05-23 12:59:00 +0200

    :rotating_light: Remove unused imports

--- app/nihilin/Main.hs
+++ app/nihilin/Main.hs
@@ -8,21 +8,15 @@ module Main
 
 import Nihil
 import Nihil.Utils.Debug
-import Nihil.Utils.Source
 import Nihil.CommonError
 import Prelude hiding (log, error, lookup, (!!))
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import Text.PrettyPrint.ANSI.Leijen (Doc, text, hardline)
+import Text.PrettyPrint.ANSI.Leijen (Doc, hardline)
 import qualified Text.PrettyPrint.ANSI.Leijen as PP (pretty)
 import System.Exit
 import Control.Monad.Except (ExceptT, runExceptT, liftEither, throwError)
-import Control.Lens ((%~), (^.))
 import Control.Monad.IO.Class (liftIO)
-import Control.Monad (when)
-import qualified Data.Map as Map
-import qualified Data.Set as Set
-import Data.Maybe (isNothing)
 
 main :: IO ()
 main = (runExceptT . workWith =<< T.getContents) >>= \case
