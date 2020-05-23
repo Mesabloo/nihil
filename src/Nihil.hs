9c5c7100a0929af7036303c994f515749c8ac9d0
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-05-23 12:32:05 +0200

    :fire: Do not re-export runtime module

--- src/Nihil.hs
+++ src/Nihil.hs
@@ -2,9 +2,7 @@ module Nihil
 ( -- * Re-exports
   module Nihil.Syntax
 , module Nihil.TypeChecking
-, module Nihil.Runtime
 ) where
 
 import Nihil.Syntax
 import Nihil.TypeChecking
-import Nihil.Runtime
