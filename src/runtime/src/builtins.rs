ac82ee9e4f518bd006ce8b78c6a2d98469843125
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-05-24 18:38:15 +0200

    :heavy_plus_sign: Add more primitive functions

--- src/runtime/src/builtins.rs
+++ src/runtime/src/builtins.rs
@@ -5,6 +5,9 @@ use std::rc::Rc;
 pub fn default_bindings() -> BTreeMap<String, Value> {
     let mut builtins = BTreeMap::new();
     builtins.insert(String::from("+"), Value::VPrim(Rc::new(add)));
+    builtins.insert(String::from("-"), Value::VPrim(Rc::new(sub)));
+    builtins.insert(String::from("*"), Value::VPrim(Rc::new(mul)));
+    builtins.insert(String::from("/"), Value::VPrim(Rc::new(div)));
 
     builtins
 }
@@ -18,9 +21,9 @@ pub fn default_constructors() -> BTreeSet<String> {
 
 //////////////////////////////////////////////////////////////////////////
 
-fn add(v1: Value, _env: &mut Environment) -> Result<Value, RuntimeError> {
-    use Value::*;
+use Value::*;
 
+fn add(v1: Value, _env: &mut Environment) -> Result<Value, RuntimeError> {
     let add_inner = move |v2: Value, _env: &mut Environment| match (&v1, v2) {
         (VInteger(x), VInteger(y)) => Ok(VInteger(x + y)),
         (VDouble(x), VDouble(y)) => Ok(VDouble(x + y)),
@@ -30,4 +33,40 @@ fn add(v1: Value, _env: &mut Environment) -> Result<Value, RuntimeError> {
     };
 
     Ok(VPrim(Rc::new(add_inner)))
+}
+
+fn sub(v1: Value, _env: &mut Environment) -> Result<Value, RuntimeError> {
+    let sub_inner = move |v2: Value, _env: &mut Environment| match (&v1, v2) {
+        (VInteger(x), VInteger(y)) => Ok(VInteger(x - y)),
+        (VDouble(x), VDouble(y)) => Ok(VDouble(x - y)),
+        (VDouble(x), VInteger(y)) => Ok(VDouble(x - (y as f64))),
+        (VInteger(x), VDouble(y)) => Ok(VDouble((*x as f64) - y)),
+        _ => Err(RuntimeError::IncorrectArguments),
+    };
+
+    Ok(VPrim(Rc::new(sub_inner)))
+}
+
+fn mul(v1: Value, _env: &mut Environment) -> Result<Value, RuntimeError> {
+    let mul_inner = move |v2: Value, _env: &mut Environment| match (&v1, v2) {
+        (VInteger(x), VInteger(y)) => Ok(VInteger(x * y)),
+        (VDouble(x), VDouble(y)) => Ok(VDouble(x * y)),
+        (VDouble(x), VInteger(y)) => Ok(VDouble(x * (y as f64))),
+        (VInteger(x), VDouble(y)) => Ok(VDouble((*x as f64) * y)),
+        _ => Err(RuntimeError::IncorrectArguments),
+    };
+
+    Ok(VPrim(Rc::new(mul_inner)))
+}
+
+fn div(v1: Value, _env: &mut Environment) -> Result<Value, RuntimeError> {
+    let div_inner = move |v2: Value, _env: &mut Environment| match (&v1, v2) {
+        (VInteger(x), VInteger(y)) => Ok(VInteger(x / y)),
+        (VDouble(x), VDouble(y)) => Ok(VDouble(x / y)),
+        (VDouble(x), VInteger(y)) => Ok(VDouble(x / (y as f64))),
+        (VInteger(x), VDouble(y)) => Ok(VDouble((*x as f64) / y)),
+        _ => Err(RuntimeError::IncorrectArguments),
+    };
+
+    Ok(VPrim(Rc::new(div_inner)))
 }
