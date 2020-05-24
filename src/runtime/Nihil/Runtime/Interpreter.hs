8c2cf565a549825ccb8c53860688ee60754fd332
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-05-24 11:32:14 +0200

    :sparkles: Allow evaluation to give default environments

--- src/runtime/Nihil/Runtime/Interpreter.hs
+++ src/runtime/Nihil/Runtime/Interpreter.hs
@@ -5,12 +5,42 @@ module Nihil.Runtime.Interpreter
 
 import Nihil.Runtime.Core
 import Foreign.Ptr (Ptr)
+import Foreign.C (CInt(..), CString, newCString)
+import Foreign.Marshal.Array
+import Foreign.Marshal.Alloc (free)
 
 foreign import ccall unsafe "runtime evaluate"
-    evaluate :: Ptr VExpr -> IO ()
+    evaluate :: Ptr VExpr -> CInt -> Ptr (Ptr C_Binding) -> CInt -> Ptr CString -> IO ()
 
-eval :: VExpr -> IO ()
-eval expr = do
+eval :: VExpr -> [(String, VExpr)] -> [String] -> IO ()
+eval expr binds cons = do
+    let nb_binds = length binds
+        nb_cons  = length cons
+
     ptr <- newExpr expr
-    evaluate ptr
+    bs  <- newBindings binds
+    cs  <- newCons cons
+
+    evaluate ptr (fromIntegral nb_binds) bs (fromIntegral nb_cons) cs
+
+    freeCons cs nb_cons
+    freeBindings bs nb_binds
     freeExpr ptr
+
+newBindings :: [(String, VExpr)] -> IO (Ptr (Ptr C_Binding))
+newBindings binds = newArray =<< traverse newBinding binds
+
+newCons :: [String] -> IO (Ptr CString)
+newCons cons = newArray =<< traverse newCString cons
+
+freeBindings :: Ptr (Ptr C_Binding) -> Int -> IO ()
+freeBindings ptr n = do
+    binds <- peekArray n ptr
+    traverse freeBinding binds
+    free ptr
+
+freeCons :: Ptr CString -> Int -> IO ()
+freeCons ptr n = do
+    cons <- peekArray n ptr
+    traverse free cons
+    free ptr
