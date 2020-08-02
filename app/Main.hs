9b4cbd9e77a2c15c40d282a1b3e0b5ada73ff41f
Mesabloo <22964017+Mesabloo@users.noreply.github.com>
2020-08-02 18:33:00 +0200

    ✅ Demonstrate the use of the CLI flag handling system

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,16 @@
 module Main where
 
-import Lib
+import Options.Applicative
+import Options.Flags
+
+import Numeric (showIntAtBase)
+import Data.Char (intToDigit)
 
 main :: IO ()
-main = someFunc
+main = do
+  flags <- customExecParser pref opts
+
+  putStrLn (showIntAtBase 2 intToDigit flags "")
+  print (isFlagOn flags flag_Wall)
+ where opts = info (pFlags <**> helper) fullDesc
+       pref = prefs showHelpOnError
